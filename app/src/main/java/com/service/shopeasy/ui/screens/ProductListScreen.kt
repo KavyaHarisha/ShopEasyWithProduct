8d0d85eb57abff31ef043cdba7171acf0fdbea33
harisha mucherla <kavya.athmuri@gmail.com>
2026-01-27 12:28:03 -0500

    Removed unused imports

--- app/src/main/java/com/service/shopeasy/ui/screens/ProductListScreen.kt
+++ app/src/main/java/com/service/shopeasy/ui/screens/ProductListScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.layout.LazyLayoutCacheWindow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
